e5a5f577909f08169e4a47c6b203a9f5df42767e
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-28 15:17:27 +0200

    Added an event listener when hovering over a square

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Selecting the HTML elements
 let gridDisplaySize = document.querySelector("#gridDisplaySize");
 let gridSize = document.querySelector("#gridSize");
+const resetBtn = document.querySelector("#reset");
+const blackBtn = document.querySelector("#black");
+const randomBtn = document.querySelector("#random");
 
 createGrid(16);
+
+// resetBtn.addEventListener("click", function () {
+//   console.log("Reset");
+// });
+// blackBtn.addEventListener("click", function () {
+//   console.log("Black");
+// });
+// randomBtn.addEventListener("click", function () {
+//   console.log("Random");
+// });
 
 // Changing the value display of the grid
 gridSize.addEventListener("change", function () {
@@ -23,11 +36,16 @@ function createGrid(size) {
   squares.forEach((div) => div.remove());
   for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
+    square.addEventListener("mouseover", function () {
+      square.classList.add("blank");
+    });
+
     square.classList.add("square");
     grid.append(square);
   }
 }
 
+// Changing the size of the board
 function changeGridSize(size) {
   createGrid(size);
 }
