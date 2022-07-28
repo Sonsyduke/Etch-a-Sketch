a1d37b3cd3191c0605d9783585510a9a5bf42e7f
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-28 14:56:00 +0200

    Changing the size of the board with the range input

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 // Selecting the HTML elements
 let gridDisplaySize = document.querySelector("#gridDisplaySize");
 let gridSize = document.querySelector("#gridSize");
-let grid = document.querySelector(".grid");
+
+createGrid(16);
 
 // Changing the value display of the grid
 gridSize.addEventListener("change", function () {
   gridDisplaySize.textContent = `${gridSize.value} x ${gridSize.value}`;
+  createGrid(gridSize.value);
 });
 
 // Creating a grid of 16x16
-createGrid();
 
-function createGrid() {
-  grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-  grid.style.gridTemplateRows = "repeat(16, 1fr)";
+function createGrid(size) {
+  let grid = document.querySelector(".grid");
 
-  for (let i = 0; i < 256; i++) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  let amount = size * size;
+
+  let squares = grid.querySelectorAll(".square");
+  squares.forEach((div) => div.remove());
+  for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
-    // square.style.backgroundColor = "black";
     square.classList.add("square");
     grid.append(square);
   }
+}
+
+function changeGridSize(size) {
+  createGrid(size);
 }
